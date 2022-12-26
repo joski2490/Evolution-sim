fdabe47d989e6d771f642d14a13cf231b68bc457
joski2490 <68449917+joski2490@users.noreply.github.com>
2022-12-26 14:16:28 -0600

    Update species.js

--- Public/js/species.js
+++ Public/js/species.js
@@ -8,7 +8,7 @@ reproduction: [% of lifespan before reproduction: Number, % chance of reproducti
 */
 
 class Species {
-    constructor(genes = { lifespan: 50, reproduction: [95, 20, 3], }, max_deviation = 25, max_population = 150, group) {
+    constructor(genes = { lifespan: 50, reproduction: [95, 20, 4], }, max_deviation = 10, max_population = 50, group) {
         this.genes = genes;
 
         this.group = group;
