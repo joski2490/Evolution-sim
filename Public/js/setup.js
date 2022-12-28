b63e24c000b0710ed176db9cda2b1635bb031586
joski2490 <68449917+joski2490@users.noreply.github.com>
2022-12-27 19:22:35 -0600

    Update setup.js

--- Public/js/setup.js
+++ Public/js/setup.js
@@ -6,9 +6,7 @@ world.addNewScene(
         scene.species = new Species({ lifespan: 25, reproduction: [5, 95, 0], }, 1, 50, group); // 100 - 30, 60, 3 - 1 - 50 - group
         scene.species.start(3, () => { return new Organism(20, 20, scene.species.color, scene.species.genes, scene.species) });
         group.add(scene.species, scene.population_counter, scene.gene_counter);
-        // org 2
-        scene.species = new Species({ lifespan: 75, reproduction: [1, 99, 0 ], }, 1, 50, group);
-        scene.species.start(3, () => { return new Organism(40, 40, scene.species.color, scene.species.genes, scene.species) });
+        
         
         scene.population_counter = Pebble.Text("Population: 0", "18px sans-serif");
         stage.putLeft(scene.population_counter, 140, -365);
